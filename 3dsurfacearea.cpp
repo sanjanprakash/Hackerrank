dcdc291b51a386720ced79ff2fc572c0d27d32e6
stanIpkiss <38526131+stanIpkiss@users.noreply.github.com>
2018-09-01 17:26:52 -0400

    3D Surface Area

---
+++ 3dsurfacearea.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int max(int a,int b) {
+    return (a < b) ? b:a;
+} 
+
+int main() {
+    int h,w;
+    cin >> h >> w;
+    int i,j,t;
+    vector<vector<int> > grid;
+    vector<int> start;
+    for (i = 0; i <= w + 1; i++) {
+        start.push_back(0);
+    }
+    grid.push_back(start);
+    for (i = 0; i < h; i++) {
+        vector<int> temp;
+        temp.push_back(0);
+        for (j = 0; j < w; j++) {
+            cin >> t;
+            temp.push_back(t);
+        }
+        temp.push_back(0);
+        grid.push_back(temp);
+    }
+    grid.push_back(start);
+    /*for (i = 0; i <= h + 1; i++) {
+        for (j = 0; j <= w + 1; j++) {
+            cout << grid[i][j] << " ";
+        }
+        cout << endl;
+    }*/
+    
+    int ans = 2 * h * w;
+    for (i = 1; i <= h; i++) {
+        for (j = 1; j <= w; j++) {
+            //cout << i << " " << j << endl;
+            ans += max(0,grid[i][j] - grid[i - 1][j]);
+            ans += max(0,grid[i][j] - grid[i + 1][j]);
+            ans += max(0,grid[i][j] - grid[i][j - 1]);
+            ans += max(0,grid[i][j] - grid[i][j + 1]);
+        }
+    }
+    cout << ans << endl;
+}
