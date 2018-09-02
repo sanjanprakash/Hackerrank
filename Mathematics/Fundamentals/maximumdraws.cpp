75f6a225826b34d732c06f036f920f942cb890af
stanIpkiss <38526131+stanIpkiss@users.noreply.github.com>
2018-09-02 11:40:02 -0400

    Maximum Draws

---
+++ Mathematics/Fundamentals/maximumdraws.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int t;
+    cin >> t;
+    while (t > 0) {
+        int n;
+        cin >> n;
+        cout << n + 1 << endl;
+        t--;
+    }
+    return 0;
+}
