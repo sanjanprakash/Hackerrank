ec108a9382774fc98758eb6574e5991b297c2510
stanIpkiss <38526131+stanIpkiss@users.noreply.github.com>
2018-09-02 16:03:13 -0400

    Weather Observation Station 11

---
+++ SQL/Basic Select/weatherobservationstation11.sql
@@ -0,0 +1,1 @@
+select distinct city from station where city not in (select city from station where (city like 'A%' or city like 'E%' or city like 'I%' or city like 'O%' or city like 'U%') AND (city like '%a' or city like '%e' or city like '%i' or city like '%o' or city like '%u'));
