fa369160a633940b520e9aca9899c7f447db0d0c
stanIpkiss <38526131+stanIpkiss@users.noreply.github.com>
2018-09-02 11:54:17 -0400

    Basic Data Types

---
+++ Languages/CPP/Introduction/datatypes.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a;
+    long b;
+    long long c;
+    char d;
+    float e;
+    double f;
+    scanf ("%d %ld %lld %c %f %lf", &a, &b, &c, &d, &e, &f);
+    printf ("%d\n", a);
+    printf ("%ld\n", b);
+    printf ("%lld\n", c);
+    printf ("%c\n", d);
+    printf ("%f\n", e);
+    printf ("%lf\n", f);
+    return 0;
+}
