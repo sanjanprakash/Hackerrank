17b697c64b0d5362f66bc59aca4baab14f790cce
stanIpkiss <38526131+stanIpkiss@users.noreply.github.com>
2018-09-01 23:13:05 -0400

    Happy Ladybugs

---
+++ Algorithms/Implementation/happyladybugs.py
@@ -0,0 +1,20 @@
+from collections import Counter
+
+for _ in range(input()):
+    n, a = input(), raw_input()
+    if a.count("_"):
+        b = Counter(a)
+        del b["_"]
+        for x in b:
+            if b[x] == 1:
+                print "NO"
+                break
+        else: print "YES"
+    else:
+        a = "_" + a + "_"
+        for i in range(1, len(a)-1):
+            if a[i] != a[i-1] and a[i] != a[i+1]:
+                print "NO"
+                break
+        else: 
+            print "YES"
