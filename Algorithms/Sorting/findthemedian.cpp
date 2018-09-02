c97114420394f7265799a5bef6c9db72b27c1c15
stanIpkiss <38526131+stanIpkiss@users.noreply.github.com>
2018-09-01 23:01:51 -0400

    Find the Median

---
+++ Algorithms/Sorting/findthemedian.cpp
@@ -0,0 +1,60 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void merge (vector<int>& original, vector<int>& first, vector<int>& second) {
+	original.clear ();															  //Deleting all the elements in the larger 'original' vector
+	int i = 0, j = 0;															   //Iterators
+	
+	//Until the end of either of the smaller vectors is reached...
+	while ((i < first.size ()) && (j < second.size ())) {
+		if (first [i] <= second [j]) {
+			original.push_back (first [i]);
+			i++;	
+		}//End if
+		else {
+			original.push_back (second [j]);
+			j++;
+		}//End else
+	}//End while
+	//After reaching the end of the 'first' vector, until the end of the 'second' vector is reached...
+	while (j < second.size ()) {
+		original.push_back (second [j]);
+		j++;
+	}//End while
+	//After reaching the end of the 'second' vector, until the end of the 'first' vector is reached...	
+	while (i < first.size ()) {
+		original.push_back (first [i]);
+		i++;
+	}//End while
+}
+
+int merge_sort (vector<int>& bigger) {
+    int mid = -99999999;
+	//If the 'bigger' vector has more than one element...
+	if (bigger.size () > 1) {
+		vector<int> first_half (bigger.begin (), bigger.begin () + (bigger.size () / 2));   //Copying first half of 'bigger' into 'first_half'
+		vector<int> second_half (bigger.begin () + (bigger.size () / 2), bigger.end ());	//Copying second half of 'bigger' into 'second_half'
+		merge_sort (first_half);															//Recursive call on 'first_half' vector
+		merge_sort (second_half);														   //Recursive call on 'second_half' vector
+		merge (bigger, first_half, second_half);		//Merging 'first_half' and 'second_half' into the original 'bigger' vector
+        mid = bigger [bigger.size () / 2];
+	}//End if
+    return mid;
+}
+
+int main() {
+    int n, temp;
+    vector <int> X;
+    cin >> n;
+    while (n > 0) {
+        cin >> temp;
+        X.push_back (temp);
+        n--;
+    }
+    cout << merge_sort (X);
+    return 0;
+}
