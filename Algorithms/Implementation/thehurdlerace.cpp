ba58eac30ccbd16dbd91d9e090a62ff702814985
stanIpkiss <38526131+stanIpkiss@users.noreply.github.com>
2018-09-01 23:55:45 -0400

    The Hurdle Race

---
+++ Algorithms/Implementation/thehurdlerace.cpp
@@ -0,0 +1,41 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int n;
+    int k;
+    cin >> n >> k;
+    vector<int> height(n);
+    for(int height_i = 0; height_i < n; height_i++){
+       cin >> height[height_i];
+    }
+    sort(height.begin(),height.end());
+    if (height[height.size() - 1] <= k)
+        cout << "0";
+    else
+        cout << height[height.size() - 1] - k;
+    return 0;
+}
