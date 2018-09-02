a4ca14ac7c434b613837e7d676cda1c1272b400e
stanIpkiss <38526131+stanIpkiss@users.noreply.github.com>
2018-09-02 11:34:56 -0400

    Binary Search Tree : Insertion

---
+++ Data Structures/Trees/insertion.cpp
@@ -0,0 +1,49 @@
+/*
+Node is defined as 
+
+typedef struct node
+{
+   int data;
+   node * left;
+   node * right;
+}node;
+
+*/
+
+
+node * insert(node * root, int value)
+{
+    if (root == NULL) {
+        root = new node;
+        root -> data = value;
+        root -> left = NULL;
+        root -> right = NULL;
+    }
+    else {
+				if (root -> data > value){
+					if (root -> left != NULL){
+						insert(root -> left, value);
+					}
+					else {
+						root -> left = new node;
+                        root -> left -> data = value;
+						root -> left -> left = NULL;
+						root -> left -> right = NULL;
+					}
+				}
+				else {
+					if (root -> right != NULL){
+						insert(root -> right, value);
+					}
+					else {
+						root -> right = new node;
+                        root -> right -> data = value;
+						root -> right -> left = NULL;
+						root -> right -> right = NULL;
+					}
+				}
+				return root;
+			}
+
+   return root;
+}
