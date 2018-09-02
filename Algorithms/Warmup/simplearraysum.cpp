a383c9b81a727113a8430e41b821f63a8eec9e3b
stanIpkiss <38526131+stanIpkiss@users.noreply.github.com>
2018-09-02 00:26:02 -0400

    Create simplearraysum.cpp

---
+++ Algorithms/Warmup/simplearraysum.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int num, sum = 0;
+    int num1;
+    cin >> num;
+    int i = 0;
+    while (i < num) {
+        cin >> num1;
+        sum = sum + num1;
+        i++;
+    }
+    cout << sum;
+    return 0;
+}
